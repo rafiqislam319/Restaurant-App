d7ca78de63f61c604ca73471913f15f06114a392
rafiqislam319 <rafiqislam319@gmail.com>
2023-02-05 12:29:19 +0600

    implement admin login system

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index()
     {
         return view('home');
+    }
+
+    public function redirects()
+    {
+        $userType = Auth::user()->user_type;
+        if ($userType == '1') {
+            return view('admin.adminHome');
+        } else {
+            return view('home');
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/redirects', [HomeController::class, 'redirects']);
 
 Route::middleware([
     'auth:sanctum',
