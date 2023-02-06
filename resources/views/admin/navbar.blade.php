a92e8f955a725854ab52c5a3b86da01281f8a81d
rafiqislam319 <rafiqislam319@gmail.com>
2023-02-06 21:39:20 +0600

    admin template setup

---
+++ resources/views/admin/navbar.blade.php
@@ -0,0 +1,54 @@
+<div class="container-scroller">
+
+    <!-- partial:partials/_sidebar.html -->
+    <nav class="sidebar sidebar-offcanvas" id="sidebar">
+
+        <ul class="nav">
+            <li class="nav-item menu-items">
+                <a class="nav-link" href="index.html">
+                    <span class="menu-icon">
+                        <i class="mdi mdi-speedometer"></i>
+                    </span>
+                    <span class="menu-title">Dashboard</span>
+                </a>
+            </li>
+
+            <li class="nav-item menu-items">
+                <a class="nav-link" href="pages/forms/basic_elements.html">
+                    <span class="menu-icon">
+                        <i class="mdi mdi-playlist-play"></i>
+                    </span>
+                    <span class="menu-title">Users</span>
+                </a>
+            </li>
+            <li class="nav-item menu-items">
+                <a class="nav-link" href="pages/tables/basic-table.html">
+                    <span class="menu-icon">
+                        <i class="mdi mdi-table-large"></i>
+                    </span>
+                    <span class="menu-title">Foods</span>
+                </a>
+            </li>
+            <li class="nav-item menu-items">
+                <a class="nav-link" href="pages/charts/chartjs.html">
+                    <span class="menu-icon">
+                        <i class="mdi mdi-chart-bar"></i>
+                    </span>
+                    <span class="menu-title">Chefs</span>
+                </a>
+            </li>
+
+            <li class="nav-item menu-items">
+                <a class="nav-link" href="http://www.bootstrapdash.com/demo/corona-free/jquery/documentation/documentation.html">
+                    <span class="menu-icon">
+                        <i class="mdi mdi-file-document-box"></i>
+                    </span>
+                    <span class="menu-title">Reservations</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <!-- partial -->
+
+    <!-- page-body-wrapper ends -->
+</div>
